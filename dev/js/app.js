c95e83eabaa3208ccae12b97be9e89d51e577e78
MilkshakePanda <iam.superpeluche@gmail.com>
2016-01-16 20:51:18 +0100

    Ajax promise done

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,11 +1,15 @@
 import {initMap} from './lib/maps';
 import {ajax} from './lib/ajax';
 
-let dribUser = "https://api.dribbble.com/v1/users/sofi_smith/"
+let dribUser = "https://api.dribbble.com/v1/users/hezy/"
 
-ajax(dribUser).then(function (data) {
+ajax(dribUser).then(function (userInfo) {
 
-    console.log(data);
+    let name = userInfo.name
+    let bio = userInfo.bio
+    let likes = userInfo.likes_count
+
+    console.log(name + " BIO: " + bio + " LIKES " + likes);
 
 }).catch(function (err) {
     console.log(err);
