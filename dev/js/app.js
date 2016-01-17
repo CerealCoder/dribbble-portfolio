c2981743fdc1ddc4426d233afd91d9eef7f1ad6b
MilkshakePanda <iam.superpeluche@gmail.com>
2016-01-17 12:57:35 +0100

    Add loading animation

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,11 +3,16 @@ import ajax, * as dribbble from './lib/ajax';
 
 
 let dribUser = "https://api.dribbble.com/v1/users/hezy/shots"
+let overlay  = document.querySelector('.loading-animation')
 
 dribbble.getShots(dribUser).then(function (shots) {
 
     dribbble.convertISOToDate(shots)
     dribbble.renderShots(shots)
+    setTimeout(function () {
+        overlay.classList.add("is-hidden")
+    }, 1000);
+
 
 }).catch(function (err) {
     console.log(err);
