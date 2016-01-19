0392b09b8688cf42fae41f1aaca5c9d4554f41ab
MilkshakePanda <iam.superpeluche@gmail.com>
2016-01-19 10:36:06 +0100

    Add general fixes

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,17 +2,19 @@ import {initMap} from './lib/maps';
 import ajax, * as dribbble from './lib/ajax';
 
 
-let dribUser = "https://api.dribbble.com/v1/users/hezy/shots"
-let overlay  = document.querySelector('.loading-animation')
+let dribUser         = "https://api.dribbble.com/v1/users/hezy/shots"
+let overlay          = document.querySelector('.loading-animation')
+let introSection     = document.querySelector('.intro')
 
 dribbble.getShots(dribUser).then(function (shots) {
 
     dribbble.convertISOToDate(shots)
     dribbble.renderShots(shots)
     setTimeout(function () {
+        document.body.classList.add("loaded")
         overlay.classList.add("is-hidden")
+        introSection.classList.add("show-meta")
     }, 1000);
-
 
 }).catch(function (err) {
     console.log(err);
